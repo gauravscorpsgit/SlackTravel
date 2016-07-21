8f2ffbc108778d17ad5bdffc1b41e4f134659332
Gaurav Chawla <gauravscorp05@gmail.com>
2016-07-21 14:47:40 +0000

    Initial commit of the app

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # SlackTravel
+
+### Run ###
+
+Run `npm install` to install the dependencies.
+
+Start the server with
+```
+node app.js
+```
+
+
+### Original source ###
---
+++ config.js
@@ -0,0 +1,11 @@
+
+var rethinkDBURL = 'https://aws-us-east-1-portal19.dblayer.com';
+var rethinkDBAuth = 'sfMFS3LZy81GK75GyH29QMXcTOXFbgrjCRHZUQs0ETI';
+
+var config = {
+    password: rethinkDBAuth,
+    username: 'compose',
+    host:rethinkDBURL,
+    srcPort: 11420,
+    dstPort: 11420
+}
